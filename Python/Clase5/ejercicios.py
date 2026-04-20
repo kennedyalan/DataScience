d7283cb86e2d816c94ceedbe6225a4c1c6216684
Alan Kennedy <Alaneduardokennedy@gmail.com>
2026-04-20 18:43:08 -0300

    ejercicio 1.60

--- Python/Clase5/ejercicios.py
+++ Python/Clase5/ejercicios.py
@@ -17,7 +17,16 @@ print(salto_de_linea(" Ejercicio - Matriz de temperaturas y filtrado con NumPy "
 # - menores a **5 °C**
 # - o mayores a **35 °C**
 temperaturas = np.random.uniform(0, 50, (30, 30))
-print(f"Matriz de temperaturas: \n{temperaturas}")
+print(f"Matriz de temperaturas: \n{np.round(temperaturas, 2)}")
 
-temperaturas_filtradas = np.round(temperaturas[(temperaturas < 5) | (temperaturas > 35)]).astype(int)
+temperaturas_filtradas = np.round(temperaturas[(temperaturas < 5) | (temperaturas > 35)], 2).astype(int)
 print(f"Temperaturas filtradas (menores a 5 °C o mayores a 35 °C): \n{temperaturas_filtradas}")
+
+
+print(salto_de_linea(" Ejercicio - 1.60 "))
+# Crea un arreglo a partir de la lista [1, 2, 3, 4, 5]. Suma 10 a cada elemento y multiplica el resultado por 2.
+
+arreglo = np.array([1, 2, 3, 4, 5])
+
+resultado = (arreglo + 10) * 2
+print(f"Resultado: \n{resultado}")
