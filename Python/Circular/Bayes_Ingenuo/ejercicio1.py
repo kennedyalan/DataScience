8cf1449791b8e3a719b8bd2190747fb01b8d5080
Alan Kennedy <Alaneduardokennedy@gmail.com>
2026-05-13 19:56:08 -0300

    feat: ejercicio 1 completion

--- Python/Circular/Bayes_Ingenuo/ejercicio1.py
+++ Python/Circular/Bayes_Ingenuo/ejercicio1.py
@@ -3,8 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-historical_clients = pd.read_csv('../Data/Datos_Historicos_Clientes.csv', sep = ";")
-new_clients = pd.read_csv('../Data/Nuevos_Clientes.csv', sep = ";")
+historical_clients = pd.read_csv('../Data/Datos_Historicos_Clientes.csv', sep = ",")
+new_clients = pd.read_csv('../Data/Nuevos_Clientes.csv', sep = ",")
 
 # Limpiar datos y convertir variables categóricas a numéricas
 historical_clients = pd.get_dummies(historical_clients, columns=['EstadoCivil', 'Profesion'])
@@ -27,3 +27,6 @@ X_new = X_new.reindex(columns=X.columns, fill_value=0)
 
 modelo = GaussianNB()
 modelo.fit(X, y)
+
+predicciones = modelo.predict(X_new)
+print(predicciones)
